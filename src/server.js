d17df88d52c9b35a14799127db142cffcf86efac
Jahoops <jwhooper@gmail.com>
2019-11-27 02:30:34 -0500

    working

--- src/server.js
+++ src/server.js
@@ -5,13 +5,58 @@ import * as sapper from '@sapper/server';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
+const Database = require('sqlite-async');
 
-polka() // You can also use Express
-	.use(
-		compression({ threshold: 0 }),
-		sirv('static', { dev }),
-		sapper.middleware()
-	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+const main = async () => {
+	let db;
+	try {
+        db = await Database.open(":memory:");
+    } catch (error) {
+        throw Error('can not access sqlite database');
+    }
+    try {
+        await db.run(`
+			CREATE TABLE user
+			(
+                firstName Text,
+                lastName Text
+			)
+    	`);	
+    } catch (error) {
+        throw Error('Could not create table')
+    }
+    try {
+        const insertString = `
+            INSERT INTO user
+            (firstName, lastName)
+            VALUES (?,?)
+        `;
+        await db.run(insertString,
+            "Michael",
+            "Lazarski"
+        );
+    } catch (error) {
+        throw Error('Could not insert new user');
+	}
+
+    polka()
+        .get('/', async (req, res) => {
+            const {firstName, lastName} = 
+            await db.get(`SELECT firstName, lastName 
+                         FROM user 
+                         WHERE firstName LIKE ?`,
+                         "%Michael%");
+            res.end(`<h1>hello ${firstName} ${lastName} </h1>`);
+		})
+		.use(
+			compression({ threshold: 0 }),
+			sirv('static', { dev }),
+			sapper.middleware()
+		)
+        .listen(3000, err => {
+            if (err) throw err;
+            console.log(`> Running on localhost:3000`);
+        });
+}
+
+main();
